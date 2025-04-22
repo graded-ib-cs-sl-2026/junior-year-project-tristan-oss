70ed364dc70191de4e217c04abc4020323f8c630
Tristan <No>
2025-04-21 23:19:50 -0300

    commit #3

--- src/App.java
+++ src/App.java
@@ -1,25 +1,30 @@
 import java.util.ArrayList; // array list instead of array because you dont know how many flashcards will be in the text file
+import java.util.Collections; // imported for shufling 
 
 public class App {
-    private InputOutput io = new InputOutput();;
-    private ArrayList<Flashcard> flashcards;
+    private InputOutput io = new InputOutput(); 
+    private ArrayList<Flashcard> flashcards; //flashcard name of array list that stores objects of the type Flashcard 
 
     public App() { 
-        this.flashcards = new ArrayList<>();
-        this.io = new InputOutput();
+        flashcards = new ArrayList<>(); // initializing flashcards 
+        
     }
 
     public void start() { 
         loadFlashcards("flashcard.txt");  
+
+        Collections.shuffle(flashcards); // shufles the array list https://www.digitalocean.com/community/tutorials/shuffle-array-java
         
         for(int i=0; i<flashcards.size(); i++) { 
-            Flashcard card = flashcards.get(i); 
-            io.output("Word: " + card.getWord());
+            Flashcard card = flashcards.get(i); // gets the i-th object of the array and stores it into card 
+            io.output("Word: " + card.getWord()); // output this to the terminal 
             io.output("press enter to see definition");
-            io.input();
+            io.input();  // waits for the user to type enter before moving on 
             io.output("definition: " + card.getDefinition());
             io.output("press enter to continue");
             io.input(); 
+
+            card.markAsStudied();
         }
         System.out.println("your done studying!");
     } 
--- src/Flashcard.java
+++ src/Flashcard.java
@@ -1,10 +1,12 @@
 public class Flashcard { 
     private String word; 
     private String definition; 
+    private boolean studied; // track if flashcard has been studied 
 
 public Flashcard(String word, String definition) { 
     this.word = word; 
     this.definition = definition;
+    studied = false;
 }
 
 public String getWord() { 
@@ -15,4 +17,10 @@ public String getDefinition() {
     return definition; 
 }
 
+public boolean isStudied() { 
+    return studied;
+}
+public void markAsStudied() { 
+    studied = true;
+}
 }
