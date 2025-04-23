6dcbf71586c956189053b01290a08da04a260342
Tristan <No>
2025-04-22 23:50:45 -0300

    Final Project Commit

--- src/App.java
+++ src/App.java
@@ -15,25 +15,66 @@ public class App {
 
         Collections.shuffle(flashcards); // shufles the array list https://www.digitalocean.com/community/tutorials/shuffle-array-java
         
+        ArrayList<Flashcard> incorrectCards = new ArrayList<>();
+
         for(int i=0; i<flashcards.size(); i++) { 
             Flashcard card = flashcards.get(i); // gets the i-th object of the array and stores it into card 
             io.output("Word: " + card.getWord()); // output this to the terminal 
             io.output("press enter to see definition");
             io.input();  // waits for the user to type enter before moving on 
             io.output("definition: " + card.getDefinition());
-            io.output("press enter to continue");
-            io.input(); 
+            io.output("did you get this definiton? (yes/no)");
+            String answer = io.input();
+            if(answer.equals("yes")) { // if statement to determine if the card is marked as studdied 
+                card.markAsStudied();
+            } else { 
+                incorrectCards.add(card); // if the user did not get the definition, that flashcard is added to the "pile"/ arrayList of incorrect ones
+            }
 
-            card.markAsStudied();
+            saveStudiedFlashcards("studied.txt"); // saves the flashcards that have been mastered up until this point 
+        
+
         }
-        System.out.println("your done studying!");
+        //this is the review section for the flashcards 
+        while(incorrectCards.size() > 0){ // while loop runs as long as their are unmastered flashcards 
+            io.output("Press enter to review the ones you missed");
+            io.input(); 
+            ArrayList<Flashcard> stillIncorrect = new ArrayList<>(); // new array list for the flachards still not mastered
+           // have to make new array list or else it creates a infinite loop ( further explanation on line 54)
+
+        for(int i = 0; i < incorrectCards.size(); i++){ 
+            Flashcard card = incorrectCards.get(i);
+            io.output("word: " + card.getWord());
+            io.output("press enter to see definition"); 
+            io.input();
+            io.output("definition: " + card.getDefinition());
+            io.output("did you get this definiton this time? (yes/no)");
+            String answer = io.input(); 
+            if(answer.equals("yes")){ 
+                card.markAsStudied();
+            } else{ 
+                stillIncorrect.add(card); // if I were to add the ones that still arent mastered back to the original incorrect pile, 
+                // it would duplicate since that card is already their. 
+
+            }
+
+        }
+        incorrectCards = stillIncorrect; // replace the old list with the new pile of unmastered
+        
+        saveStudiedFlashcards("studied.txt"); // save the mastered flashcards after reviewing the incorrect ones
+
+
+        }
+
+        System.out.println("your done studying!"); 
+       
     } 
     
 
 
-    
+    /** reads the text file with the flashcards. Text file Must have word on one line, then definition on the line below. Then another word below the definiton and so on. No spaced between lines*/
     public void loadFlashcards(String filename) {
-        try{ 
+        try{ // used Mr Griswolds code along with this link to understand this. https://www.w3schools.com/java/java_try_catch.asp
             io.openFile(filename);
         } catch (Exception e) { 
             io.output("Error reading File");
@@ -43,12 +84,23 @@ public class App {
                 String word =io.getNextLine();
                 if (io.fileHasNextLine()) { 
                     String definition = io.getNextLine();
-                    flashcards.add(new Flashcard(word, definition));
+                    flashcards.add(new Flashcard(word, definition)); // this line adds the flashcard with word and definition to the array list. Learned from https://stackoverflow.com/questions/19616972/how-to-add-an-object-to-an-arraylist-in-java
                 }
             }
             
         }
-
+        /** saves the flashcards that have been studdied into a separate text file */
+public void saveStudiedFlashcards(String filename) { 
+    io.openWriteFile(filename);
+    for(int i = 0; i < flashcards.size(); i++) {
+        Flashcard card = flashcards.get(i);
+        if(card.isStudied()) { 
+            io.writeToFile(card.getWord());; // if card is studied it will write the word onto the studied file 
+            io.writeToFile(card.getDefinition()); // then it will write the definition of that word to the studied file 
+        } 
+    }
+io.closeWriteFile(); 
+}
 
     
         public static void main(String[] args) {
--- src/Flashcard.java
+++ src/Flashcard.java
@@ -2,24 +2,25 @@ public class Flashcard {
     private String word; 
     private String definition; 
     private boolean studied; // track if flashcard has been studied 
-
+/** constructor for flashcard */
 public Flashcard(String word, String definition) { 
-    this.word = word; 
-    this.definition = definition;
+    this.word = word; // use this. since parameter and instance variable have the same name. chatGPT helped me with this https://docs.google.com/document/d/1ONCWdZvLwDbZJ-81M9bXXRUlULQJ165AYNjuXBuc1uc/edit?tab=t.0
+    this.definition = definition; // looking back could have just changed the names
     studied = false;
 }
-
+/** getter to access the word of the flash card  */
 public String getWord() { 
     return word;
 }
-
+/** getter for the definition on the flashcard  */
 public String getDefinition() { 
     return definition; 
 }
-
+/** checks if the flashcard has been studied, returns true or false */
 public boolean isStudied() { 
     return studied;
 }
+/** marks the flashcard as studied, change studied boolean to true */
 public void markAsStudied() { 
     studied = true;
 }
